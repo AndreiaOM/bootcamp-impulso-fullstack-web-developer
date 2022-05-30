66079424c559915bff9f869c9ba982d0ca39fe2c
Andréia Macedo <andreiaolimacedo@gmail.com>
2022-05-29 21:23:04 -0300

    Delete Desafio de Código (básico) directory

--- Desafio de Código (básico)/Batmain/index.js
+++
@@ -1,6 +0,0 @@
-let n = parseInt(gets());
-
-for (let i = 0; i < n; i++) {
-	VilaoPego = gets();
-	print("Y");
-}
--- Desafio de Código (básico)/Nome no Formulário/index.js
+++
@@ -1,4 +0,0 @@
-let L = gets()
-
-if (L.length <= 80) {
-  print("YES") } else {print("NO")};
